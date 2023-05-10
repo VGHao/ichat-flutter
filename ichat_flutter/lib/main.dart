8eb76bbb9a24f391db0cf98561daa1797fcab0a9
VGHao <50015112+VGHao@users.noreply.github.com>
2023-05-10 15:48:38 +0700

    Setup Firebase and add launcher icon

--- ichat_flutter/lib/main.dart
+++ ichat_flutter/lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ichat_flutter/firebase_options.dart';
 
 import 'colors.dart';
 import 'screens/mobile_layout_screen.dart';
 import 'screens/web_layout_screen.dart';
 import 'utils/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
