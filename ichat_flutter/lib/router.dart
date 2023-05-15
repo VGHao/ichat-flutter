05d1fc03233b48d643678521d81f9c4a20413a36
VGHao <50015112+VGHao@users.noreply.github.com>
2023-05-15 14:52:19 +0700

    Setup Firebase for phone login

--- ichat_flutter/lib/router.dart
+++ ichat_flutter/lib/router.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:ichat_flutter/common/widgets/error.dart';
 import 'package:ichat_flutter/features/auth/screens/login_screen.dart';
+import 'package:ichat_flutter/features/auth/screens/otp_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case LoginScreen.routeName:
-      return MaterialPageRoute(builder: (context) => const LoginScreen());
+      return MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      );
+    case OTPScreen.routeName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (context) => OTPScreen(
+          verificationId: verificationId,
+        ),
+      );
     default:
       return MaterialPageRoute(
         builder: (context) =>
